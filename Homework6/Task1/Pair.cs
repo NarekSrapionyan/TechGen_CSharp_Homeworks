a54b25bfcb13511bce18b29e01d7c192c33c536e
Narek Srapionyan <n.srapionyan05@gmail.com>
2026-06-30 00:37:25 +0400

    Nice!

---
+++ Homework6/Task1/Pair.cs
@@ -0,0 +1,23 @@
+﻿namespace Task1;
+
+public class Pair<T1, T2>
+{
+    public readonly  T1 First;
+    public readonly T2 Second;
+
+    public Pair(T1 first, T2 second)
+    {
+        First = first;
+        Second = second;
+    }
+
+    public Pair<T2, T1> SwapSides()
+    {
+        return new Pair<T2, T1>(Second, First);
+    }
+    
+    public override string ToString()
+    {
+        return $"({First}, {Second})";
+    }
+}
---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pair<int,string> pair1 = new Pair<int,string>(1, "One");
+        Pair<string, int> swapped = pair1.SwapSides();
+        Console.WriteLine($"Original Pair : {pair1}");
+        Console.WriteLine($"Swapped Pair  : {swapped}");
+        
+    }
+}
