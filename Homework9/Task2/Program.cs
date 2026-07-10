3881532233e7e78250d689915d4f0e4d3523d834
Narek Srapionyan <n.srapionyan05@gmail.com>
2026-07-10 23:57:53 +0400

    Nice!

---
+++ Homework9/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inbox = "inbox";
+
+        Directory.CreateDirectory(inbox);
+
+        int count = 0;
+
+        foreach (string file in Directory.EnumerateFiles(inbox))
+        {
+            FileInfo fileInfo = new FileInfo(file);
+
+            Console.WriteLine($"File : {Path.GetFileName(file)}");
+            Console.WriteLine($"Size : {fileInfo.Length} bytes");
+
+            count++;
+        }
+
+        Console.WriteLine($"Count of Files: {count}");    
+        
+    }
+}
