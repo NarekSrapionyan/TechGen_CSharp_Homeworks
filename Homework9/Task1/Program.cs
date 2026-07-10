e1c37db1fa656b0656e625c3551cce571408e0b2
Narek Srapionyan <n.srapionyan05@gmail.com>
2026-07-10 23:47:20 +0400

    Nice!

---
+++ Homework9/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string folderName = "reports";
+        Directory.CreateDirectory(folderName);
+
+        string path = Path.Combine(folderName, "report.txt");
+        string reportContent = "asdadsadlkajad asdada\n asdasdad asdasd\n asdasdasd asdasdasda\n adasdadsda \n";
+
+        File.WriteAllText(path, reportContent);
+        string readContent = File.ReadAllText(path);
+
+        if (reportContent == readContent)
+        {
+            Console.WriteLine("Saved!\n");
+        }
+        else
+        {
+            Console.WriteLine("Failed!\n");
+        }
+    }
+}
